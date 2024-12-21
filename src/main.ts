840c2f0284647ac022740b6f6c57fcdf647d131f
luizgcl <luiizgcl@gmail.com>
2024-12-21 14:21:02 -0300

    add global exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggerInterceptor } from './common/interceptors/logger/logger.interceptor';
+import { GlobalExceptionFilter } from './common/filters/exception/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new LoggerInterceptor());
+  app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
